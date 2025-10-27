acc426785ebc12b6acbaf5e6ba69f68f0468d430
Stuart-Roberts <stuartroberts@me.com>
2025-10-27 23:32:30 +0000

    Upgrade to v2.0 Universal Framework - multi-domain headline generation

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
