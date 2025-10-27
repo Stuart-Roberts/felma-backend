446583b112a3d20aac72b4f07511f0dc3cbc37d0
Stuart-Roberts <stuartroberts@me.com>
2025-10-27 23:32:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
